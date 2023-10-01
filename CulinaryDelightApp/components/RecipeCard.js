0c8f226c2823021aa15d3a59d9d23417be32f61a
FriedrichMatheus <camposfriedrichmatheus@gmail.com>
2023-09-30 22:06:03 -0300

    ta ae

--- CulinaryDelightApp/components/RecipeCard.js
+++ CulinaryDelightApp/components/RecipeCard.js
@@ -2,115 +2,97 @@
 import { Text, View, Pressable, ImageBackground } from "react-native";
 import style from "../styles/styles";
 import { useEffect, useState } from "react";
-import queryDB, {
-  allRecipes,
-  downloadLinkList,
-  getImages,
-} from "../services/dbRead";
+import getRecipesList, { getImages } from "../services/dbRead";
 
 function RecipeCard() {
-  const [count, setCount] = useState(0);
   const [info, setInfo] = useState();
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([""]);
 
-  let filteredRecipeData = [];
+  async function fetchRecipesList() {
+    const recipesList = await getRecipesList();
 
-  async function fetchData() {
-    if (info) {
-      return null;
-    } else {
-      await queryDB();
+    const data = recipesList.map((filteredData) => {
+      return filteredData.data();
+    });
 
-      allRecipes.map((filteredData) => {
-        filteredRecipeData.push(filteredData.data());
-      });
-
-      setInfo(filteredRecipeData);
-    }
+    setInfo(data);
   }
 
   async function fetchImages() {
-    if (images) {
-      return null;
-    } else {
-      const imagesList = await getImages();
+    const imagesList = await getImages();
 
-      console.log("imagesList", imagesList);
-    }
+    setImages(imagesList);
+  }
 
-    useEffect(() => {
-      fetchData();
-      fetchImages();
-    });
+  useEffect(() => {
+    fetchRecipesList();
+    fetchImages();
+  }, []);
 
-    if (info && images) {
-      return info.map((recipe, index) => {
-        console.log(images[index]);
-
-        return (
-          <View key={index} style={style.recipeCard}>
-            <View style={style.recipeAuthor}>
-              <View style={style.recipeProfilePhoto}></View>
-              <Text
-                style={[
-                  style.commonTextColor,
-                  { fontFamily: "MontserratExtraBold" },
-                ]}
-              >
-                {recipe.Autor}
-              </Text>
-            </View>
-            <Text
-              style={[
-                style.commonTextColor,
-                { fontFamily: "MontserratBlack", fontSize: 35 },
-              ]}
-            >
-              {recipe.Titulo}
-            </Text>
-            <Text
-              style={[
-                style.commonTextColor,
-                { fontFamily: "MontserratMedium", fontSize: 12 },
-              ]}
-            >
-              {recipe.data}
-            </Text>
-            <View style={style.recipeInformationContainer}>
-              <View style={style.recipeImage}>
-                <ImageBackground resizeMode="cover"></ImageBackground>
-              </View>
-              <Text></Text>
-              <View style={style.infoContainer}>
-                <View>
-                  <Text style={style.cardInfoTitle}>Descrição</Text>
-                  <Text style={style.info}>{recipe.Descricao}</Text>
-                </View>
-                <View>
-                  <Text style={style.cardInfoTitle}>
-                    Lista de Ingredientes:
-                  </Text>
-                  <Text style={style.info}>
-                    {recipe.Ingredientes.map((ingrediente) => {
-                      return `${ingrediente}; `;
-                    })}
-                  </Text>
-                  <Pressable>
-                    <Text style={style.hiperlink}>ver receita...</Text>
-                  </Pressable>
-                </View>
-              </View>
+  return info?.map((recipe, index) => {
+    return (
+      <View key={index} style={style.recipeCard}>
+        <View style={style.recipeAuthor}>
+          <View style={style.recipeProfilePhoto}></View>
+          <Text
+            style={[
+              style.commonTextColor,
+              { fontFamily: "MontserratExtraBold" },
+            ]}
+          >
+            {recipe.Autor}
+          </Text>
+        </View>
+        <Text
+          style={[
+            style.commonTextColor,
+            { fontFamily: "MontserratBlack", fontSize: 35 },
+          ]}
+        >
+          {recipe.Titulo}
+        </Text>
+        <Text
+          style={[
+            style.commonTextColor,
+            { fontFamily: "MontserratMedium", fontSize: 12 },
+          ]}
+        >
+          {recipe.data}
+        </Text>
+        <View style={style.recipeInformationContainer}>
+          <View style={style.recipeImage}>
+            <ImageBackground
+              style={{ width: "100%", height: "100%" }}
+              source={{
+                uri: images[index],
+              }}
+            />
+          </View>
+          <Text></Text>
+          <View style={style.infoContainer}>
+            <View>
+              <Text style={style.cardInfoTitle}>Descrição</Text>
+              <Text style={style.info}>{recipe.Descricao}</Text>
             </View>
             <View>
-              <Text></Text>
+              <Text style={style.cardInfoTitle}>Lista de Ingredientes:</Text>
+              <Text style={style.info}>
+                {recipe.Ingredientes.map((ingrediente) => {
+                  return `${ingrediente}; `;
+                })}
+              </Text>
+              <Pressable>
+                <Text style={style.hiperlink}>ver receita...</Text>
+              </Pressable>
             </View>
           </View>
-        );
-      });
-    } else {
-      return <Text>Deu Ruim</Text>;
-    }
-  }
+        </View>
+        <View>
+          <Text></Text>
+        </View>
+      </View>
+    );
+  });
 }
 
 export default RecipeCard;
